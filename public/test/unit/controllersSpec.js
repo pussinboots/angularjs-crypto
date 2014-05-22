f8aba4e87d9b1c7dfe73d77e6c7f356c609c874f
pussinboots <pussinboots666@googlemail.com>
2014-05-22 16:57:11 +0200

    - setup karma unit test

--- public/test/unit/controllersSpec.js
+++ public/test/unit/controllersSpec.js
@@ -18,6 +18,12 @@ describe('Controllers tests', function () {
     describe('ConfigCtrl', function () {
         var scope, $httpBackend, rootScope;
 
+	beforeEach(inject(function (_$httpBackend_, $rootScope, $controller) {
+            $httpBackend = _$httpBackend_;
+            scope = $rootScope.$new();
+            $controller(ProfileCtrl, {$scope: scope});
+        }));
+
         it('should set result of profile response to the scope result variable', function () {
             $httpBackend.expectGET('/assets/config').respond(200,[{name:"XJWoMnnOlSF3tFoU4jn4gg==", value: "l0gZvr5oiHds8nQpqe0Kqg=="}]);
             rootScope.$digest()
