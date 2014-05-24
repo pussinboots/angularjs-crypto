5836bc3526dbb7d3b03b794006d6592fa6473515
pussinboots <pussinboots666@googlemail.com>
2014-05-24 13:47:50 +0200

    - use the port offered from heroku or if not specified than 9000

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var connect = require('connect');
+var port = Number(process.env.PORT || 9000);
 var server = connect().
     use(connect.static(__dirname+'/public')).
-    listen(9000);
+    listen(port);
