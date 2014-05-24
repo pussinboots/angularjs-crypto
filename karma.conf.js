c92300264e5a942fca909acc056a0edff5bccdf2
pussinboots <pussinboots666@googlemail.com>
2014-05-24 17:14:25 +0200

    - activate Chrome browser for karma tests

--- karma.conf.js
+++ karma.conf.js
@@ -75,7 +75,7 @@ module.exports = function (config) {
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers: ['Firefox','Opera','PhantomJS'],
+        browsers: ['Firefox','Opera','Chrome'],
 
 
         // If browser does not capture in given timeout [ms], kill it
