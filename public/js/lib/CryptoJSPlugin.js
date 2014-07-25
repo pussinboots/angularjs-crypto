c4710e367a204cc0078a4cedaa80d465c1b075fa
pussinboots <pussinboots666@googlemail.com>
2014-07-25 23:23:13 +0000

    Code cleanup.

--- public/js/lib/CryptoJSPlugin.js
+++ public/js/lib/CryptoJSPlugin.js
@@ -7,15 +7,12 @@ function CryptoJSAES(mode, padding) {
                     var self = this;
                     if (!plainValue) { return plainValue; }
                     if (base64Key.length<=0) return plainValue;
-                    //TODO make key configurable
-                    //var base64Key = rootScope.baseKey;//"16rdKQfqN3L4TY7YktgxBw==";
-                    //console.log( "base64Key = " + base64Key );
-
+                    
                     var key = CryptoJS.enc.Base64.parse(base64Key);
                     // this is the decrypted data as a sequence of bytes
                     var encryprtedData = CryptoJS.AES.encrypt(plainValue, key, {
-                        mode: mode, //CryptoJS.mode.ECB,
-                        padding: padding //CryptoJS.pad.Pkcs7
+                        mode: mode,
+                        padding: padding
                     });
 
                     //var encryprtedValue = encryprtedData.toString( CryptoJS.enc.Base64);
@@ -23,15 +20,12 @@ function CryptoJSAES(mode, padding) {
                 },
         decode : function(encryptedValue, base64Key) {
                     var self = this;
-                    //TODO make key configurable
-                    //var base64Key = rootScope.baseKey;//"16rdKQfqN3L4TY7YktgxBw==";
-                    //console.log( "base64Key = " + base64Key );
                     if (base64Key.length<=0) return encryptedValue;
                     var key = CryptoJS.enc.Base64.parse(base64Key);
                     // this is the decrypted data as a sequence of bytes
                     var decryptedData = CryptoJS.AES.decrypt(encryptedValue, key, {
-                        mode: mode, //CryptoJS.mode.ECB,
-                        padding: padding //CryptoJS.pad.Pkcs7
+                        mode: mode,
+                        padding: padding
                     });
                     return  decryptedData.toString(CryptoJS.enc.Utf8);
                 }
