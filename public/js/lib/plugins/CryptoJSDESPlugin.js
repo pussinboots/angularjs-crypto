043930a69f9b771ef66b73eac55c1728cae7ad9b
Frank <pussinboots666@googlemail.com>
2014-10-06 20:17:15 +0200

    - renamed to CryptoJSDES class

--- public/js/lib/plugins/CryptoJSDESPlugin.js
+++ public/js/lib/plugins/CryptoJSDESPlugin.js
@@ -1,5 +1,5 @@
 'use strict';
-function CryptoJSAES(mode, padding) {
+function CryptoJSDES(mode, padding) {
     var mode = mode;
     var padding = padding;
     return {
@@ -9,8 +9,10 @@ function CryptoJSAES(mode, padding) {
                     
                     var key = CryptoJS.enc.Base64.parse(base64Key);
                     // this is the decrypted data as a sequence of bytes
-                    var encryprtedData = CryptoJS.DES.encrypt(plainValue, key);
-
+                    var encryprtedData = CryptoJS.DES.encrypt(plainValue, key, {
+                        mode: mode,
+                        padding: padding
+                    });
                     //var encryprtedValue = encryprtedData.toString( CryptoJS.enc.Base64);
                     return encryprtedData.toString();
                 },
@@ -18,7 +20,10 @@ function CryptoJSAES(mode, padding) {
                     if (base64Key.length<=0) return encryptedValue;
                     var key = CryptoJS.enc.Base64.parse(base64Key);
                     // this is the decrypted data as a sequence of bytes
-                    var decryptedData = CryptoJS.DES.decrypt(encryptedValue, key);
+                    var decryptedData = CryptoJS.DES.decrypt(encryptedValue, key, {
+                        mode: mode,
+                        padding: padding
+                    });
                     return  decryptedData.toString(CryptoJS.enc.Utf8);
                 }
     }
