26e751f2340bd1b877c812bcffe6f1e6a807386c
Frank <pussinboots666@googlemail.com>
2014-10-06 20:13:56 +0200

    - copy of services.js for des example and tests

---
+++ public/js/des/services.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/* Services */
+
+angular.module('services', ['ngResource'], function ($provide) {
+
+    $provide.factory('Data', function ($resource) {
+        return $resource('/assets/config', {}, {
+            query: {method: 'GET', isArray: false, crypt: true},
+            queryFullCrypt: {method: 'GET', isArray: false, fullcryptquery:true},
+            queryNoCrypt: {method: 'GET'},
+            save: {method: 'POST', crypt: true},
+            saveNoCrypt: {method: 'POST'},
+            saveFullCrypt: {method: 'POST',  fullcryptbody:true}
+        });
+    });
+
+    $provide.factory('Empty', function ($resource) {
+        return $resource('/assets/empty', {}, {
+            post: {method: 'POST', crypt: true}
+        });
+    });
+});
