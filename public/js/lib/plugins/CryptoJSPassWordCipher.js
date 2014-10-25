c7f2f7836d18b34a588bcb2de9fc43fb0a2b9d68
Frank <pussinboots666@googlemail.com>
2014-10-25 21:17:53 +0200

    - support now password based key generation

---
+++ public/js/lib/plugins/CryptoJSPassWordCipher.js
@@ -0,0 +1,30 @@
+'use strict';
+function CryptoJSCipher(mode, padding, Cipher) {
+    var mode = mode;
+    var padding = padding;
+    var Cipher = Cipher;
+    return {
+        encode: function(plainValue, password) {
+                    if (!plainValue) { return plainValue; }
+                    if (base64Key.length<=0) return plainValue;
+                    var key = CryptoJS.SHA3(password, { outputLength: 256 });
+                    // this is the decrypted data as a sequence of bytes
+                    var encryprtedData = Cipher.encrypt(plainValue, key, {
+                        mode: mode,
+                        padding: padding
+                    });
+                    //var encryprtedValue = encryprtedData.toString( CryptoJS.enc.Base64);
+                    return encryprtedData.toString();
+                },
+        decode : function(encryptedValue, password) {
+                    if (base64Key.length<=0) return encryptedValue;
+                    var key = CryptoJS.SHA3(password, { outputLength: 256 });
+                    // this is the decrypted data as a sequence of bytes
+                    var decryptedData = Cipher.decrypt(encryptedValue, key, {
+                        mode: mode,
+                        padding: padding
+                    });
+                    return  decryptedData.toString(CryptoJS.enc.Utf8);
+                }
+    }
+}
