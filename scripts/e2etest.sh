8e4ca98411fef0896acb4eb7822cc65ed6c3beb8
vagrant <pussinboots666@googlemail.com>
2014-11-09 11:34:27 +0000

    - fix permission deny on travis build
    See http://www.kypertrast.net/seneca/2012/10/26/more-travis-ci-testing-installing-modules-and-running-shell-scripts/.

--- scripts/e2etest.sh
+++ scripts/e2etest.sh
@@ -2,4 +2,4 @@
 node server.js&>application.log
 scripts/karma.sh e2e
 #stop node server not nice but worked
-kill -9 $(ps aux | grep '\snode\s' | awk '{print $2}')
+#kill -9 $(ps aux | grep '\snode\s' | awk '{print $2}')
