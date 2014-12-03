4b7d6fba3508fca47305e5dcbd84dc2b1e4e7c2b
Frank <pussinboots666@googlemail.com>
2014-12-03 08:52:36 +0100

    implements echo server

--- server.js
+++ server.js
@@ -2,5 +2,11 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 var port = Number(process.env.PORT || 9000);
 connect().
+    use('/data', function fooMiddleware(req, res, next) {
+          res.writeHead(200);
+          req.pipe(res);
+          next();
+        }
+    ).
     use(serveStatic(__dirname+'/public')).
     listen(port);
