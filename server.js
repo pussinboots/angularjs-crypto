c69ba072b3e3bd71fe1a2ed756be441b12aa2d83
Frank <pussinboots666@googlemail.com>
2014-12-04 08:29:06 +0100

    implements mock get request

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 var port = Number(process.env.PORT || 9000);
 connect().
-    use('/data', function fooMiddleware(req, res, next) {
-          res.writeHead(200);
-          req.pipe(res);
-          next();
-        }
-    ).
+    use('/data/get/aes', function getaes(req, res, next) {
+        var obj = {    items: [
+                    {name_enc: "XJWoMnnOlSF3tFoU4jn4gg==", value_enc: "l0gZvr5oiHds8nQpqe0Kqg==", plain: "Hallo"},
+                    {name_enc: "XJWoMnnOlSF3tFoU4jn4gg==", value_enc: "l0gZvr5oiHds8nQpqe0Kqg==", plain: "Hallo"},
+                    {name_enc: "XJWoMnnOlSF3tFoU4jn4gg==", value_enc: "l0gZvr5oiHds8nQpqe0Kqg=="}
+                ],
+                    count: 3
+                };
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify(obj));    
+        next();
+    }).
+    use('/data', function echo(req, res, next) {
+        res.writeHead(200);
+        req.pipe(res);
+        next();
+    }).
     use(serveStatic(__dirname+'/public')).
     listen(port);
