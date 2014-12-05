72dfe7bab6db9a39aea85f68ad93ec7fbcfe8d8b
Frank <pussinboots666@googlemail.com>
2014-12-05 06:41:25 +0100

    Update services.js

--- public/js/services.js
+++ public/js/services.js
@@ -6,7 +6,7 @@ angular.module('services', ['ngResource'], function ($provide) {
 
     $provide.factory('Data', function ($resource) {
         return $resource('/assets/config', {}, {
-            query: {method: 'GET', isArray: false, crypt: true},
+            query: {url:'/data/get/aes', method: 'GET', isArray: false, crypt: true},
             queryFullCrypt: {method: 'GET', isArray: false, fullcryptquery:true},
             queryNoCrypt: {method: 'GET'},
             save: {method: 'POST', crypt: true},
