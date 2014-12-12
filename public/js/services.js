dfe0bbd8e8f7eddc9fbddcc2294b1e2361d8083f
Frank <pussinboots666@googlemail.com>
2014-12-12 08:46:48 +0100

    Update services.js

--- public/js/services.js
+++ public/js/services.js
@@ -19,10 +19,4 @@ angular.module('services', ['ngResource'], function ($provide) {
             }
         });
     });
-
-    $provide.factory('Empty', function ($resource) {
-        return $resource('/assets/empty', {}, {
-            post: {method: 'POST', crypt: true}
-        });
-    });
 });
