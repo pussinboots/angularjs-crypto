b633f8ec9b491a7d083b7567dad6215da1a414d7
pussinboots <pussinboots666@googlemail.com>
2015-01-17 00:50:07 +0000

    Added karma config for e2e test inside codio.

---
+++ karma/karma.conf-e2e-codio.js
@@ -0,0 +1,27 @@
+var sharedConfig = require('./karma.conf-saucelabs.js');
+var hostname = require('os').hostname() + '.codio.io';
+var proxyHost = 'http://' + hostname + ':9000/';
+
+module.exports = function (config) {
+    sharedConfig(config);
+    config.files.push('public/test/e2e/*.coffee');
+
+    config.set({
+        files: [
+            'node_modules/karma-ng-scenario/lib/angular-scenario.js',
+            'node_modules/karma-ng-scenario/lib/adapter.js',
+            'public/test/e2e/*.coffee',
+        ],
+        reporters: ['progress', 'dots', 'junit'],
+    	junitReporter: {
+			outputFile: 'test-results-e2e.xml',
+			suite: 'End2End test'
+		},
+        port: 9002,
+        	browsers: ['PhantomJS'],
+        hostname: hostname,
+        proxies: {
+            '/': proxyHost
+        }
+    });
+}
