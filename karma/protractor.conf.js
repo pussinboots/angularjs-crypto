59bd84c6e9254e794c71926a0228b6fca0283cb3
Frank <pussinboots666@googlemail.com>
2015-02-09 13:34:53 +0000

    Upload protractor test results to unitcover.

--- karma/protractor.conf.js
+++ karma/protractor.conf.js
@@ -23,4 +23,13 @@ exports.config = {
   ],
   framework: 'jasmine',
   baseUrl: 'http://localhost:9000/',
+  onPrepare: function() {
+    // The require statement must be down here, since jasmine-reporters
+    // needs jasmine to be in the global and protractor does not guarantee
+    // this until inside the onPrepare function.
+    require('jasmine-reporters');
+    jasmine.getEnv().addReporter(
+      new jasmine.JUnitXmlReporter('./', true, true, 'protractor.', true)
+    );
+  },
 }
