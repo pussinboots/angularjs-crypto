43a559db9e530c9ce09efe883a781fbda187a081
Frank <pussinboots666@googlemail.com>
2015-02-09 11:34:47 +0000

    Added sleep between selenium start and protractor run.

--- scripts/e2etestprotractor.sh
+++ scripts/e2etestprotractor.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 node server.js&>application.log
 scripts/selenium.sh &
+sleep 5
 scripts/protractor.sh
 #stop node server not nice but worked
 kill -9 $(ps aux | grep '\snode\s' | awk '{print $2}')
