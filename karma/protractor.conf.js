330ddd69e85ce485a7e12de5b9ec37fc107fd12d
Frank <pussinboots666@googlemail.com>
2015-02-09 13:53:28 +0100

    set no-sandbox args for chrome

--- karma/protractor.conf.js
+++ karma/protractor.conf.js
@@ -13,7 +13,10 @@ exports.config = {
       'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
   },*/
   capabilities: {
-      'browserName': 'chrome'
+      'browserName': 'chrome',
+      'chromeOptions': {
+        'args': ['no-sandbox']
+      }
   },
   specs: [
       '../public/test/e2e/protractor/*.coffee'
