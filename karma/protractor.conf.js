458d723fc39910a70f28092074130ec62e072b65
Frank <pussinboots666@googlemail.com>
2015-03-20 10:57:40 +0000

    Set timeout for protractor to 20 seconds.

--- karma/protractor.conf.js
+++ karma/protractor.conf.js
@@ -1,17 +1,7 @@
 require('coffee-script').register();
-//var hostname = require('os').hostname() + '.codio.io';
-/*var hostname = 'localhost';
-var proxyHost = 'http://' + hostname + ':9000/';
-*/
 
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  //baseUrl : proxyHost,
-  /*capabilities: {
-      'browserName': 'phantomjs',
-      'phantomjs.binary.path': 'node_modules/karma-phantomjs-launcher/node_modules/phantomjs/bin',
-      'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
-  },*/
   capabilities: {
       'browserName': 'chrome',
       'chromeOptions': {
@@ -22,6 +12,7 @@ exports.config = {
       '../public/test/e2e/protractor/*.coffee'
   ],
   framework: 'jasmine',
+  allScriptsTimeout: 20000,
   baseUrl: 'http://localhost:9000/',
   onPrepare: function() {
     // The require statement must be down here, since jasmine-reporters
