39105c3e771ddd5716421ac195d5ea60d49a2857
Frank <pussinboots666@googlemail.com>
2016-03-19 18:19:47 +0100

    try to fix failed test

--- public/js/lib/angularjs-crypto.js
+++ public/js/lib/angularjs-crypto.js
@@ -37,7 +37,7 @@ cryptoModule.config(['$httpProvider', function ($httpProvider) {
             },
             response: function (response) {
                 var shouldCrypt = (response.config || false).crypt  && defaultVal(response.config.decrypt, true);
-                var pattern = (cfg).pattern;
+                var pattern = (response.config)? response.config.pattern : cfg.pattern);
                 if (missingCryptoJs(shouldCrypt, cfg, $q)) {
                     return q.reject('CryptoJS missing');
                 }
